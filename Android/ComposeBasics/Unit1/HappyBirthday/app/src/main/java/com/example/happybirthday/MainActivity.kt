559017da35c2f76320633c1acf24e38334dc59b2
Paul Williams <thecodinganthropoid@gmail.com>
2023-06-16 08:11:55 +0100

    feat(U1-HappyBirthday): pathway 3, step 4
    - add image
    - modify image display type
    - extract strings

--- Android/ComposeBasics/Unit1/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Android/ComposeBasics/Unit1/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,7 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,12 +31,12 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+//                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Salubrious Salutations",
-                        sender = "The Lord High Commander",
+                    GreetingImage(
+                        message = getString(R.string.happy_birthday_text),
+                        sender = getString(R.string.sender),
                     )
 
                 }
@@ -54,41 +57,72 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, sender: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp).background(Color.Green),
+        modifier = modifier
+            .padding(8.dp)
+//            .background(Color.Green)
     ) {
         Text(
             text = message,
             fontSize = 72.sp,
             lineHeight = 76.sp,
             textAlign = TextAlign.Center,
-            modifier = Modifier
-                .background(Color.Red)
+//            modifier = Modifier
+//                .background(Color.Red)
         )
         Text(
-            text = "From\n$sender",
+            text = buildString {
+                append(stringResource(R.string.from))
+                append(sender)
+            },
             fontSize = 28.sp,
             lineHeight = 32.sp,
-            textAlign = TextAlign.End,                  // Aligns Text in box
+//            textAlign = TextAlign.Center,               // Center text method 1
+//            textAlign = TextAlign.End,                  // Aligns Text in box
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
-                .background(Color.Blue)                 // Aligns Box relative to parent
+                .align(alignment = Alignment.CenterHorizontally)    // Center text method 2
+//                .fillMaxWidth()                         // Center text method 1
+//                .align(alignment = Alignment.End)       // Aligns Box relative to parent
+//                .background(Color.Blue)
+            /*
+            Method 1 sets the text container to be max width and centers the text inside the
+            container.
+            Method 2 centers the text container in the column.
+             */
         )
     }
 }
 
-
+@Composable
+fun GreetingImage(message: String, sender: String, modifier: Modifier = Modifier) {
+    val backgroundImage = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = backgroundImage,
+            contentDescription = stringResource(R.string.bg_img_description),
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f,
+        )
+        GreetingText(
+            message = message,
+            sender = sender,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
 
 @Preview(
-    showBackground = true,
+    showBackground = false,
     showSystemUi = true,
     name = "My Preview",
-    )
+)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-
-        GreetingText("Happy Birthday Sam!", "Emma Stone",)
+        GreetingImage(message = "Happy Birthday Sam!", sender = "Emma")
+//        GreetingText("Happy Birthday Sam!", "Emma Stone",)
 //        GreetingText(LoremIpsum(1000).values.joinToString(), "")
     }
 }
